36ba8480d774a57dd27689912c4a473512437860
Alexandre <alexandreaanp@gmail.com>
2025-03-31 23:00:56 +0100

    feat(turbo): update configuration and package structure

--- functions/session/src/functions/getSession.ts
+++ functions/session/src/functions/getSession.ts
@@ -1,8 +1,8 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-import { Session } from "../../../../packages/models"; // Adjust the import path as necessary
+import { Session } from "@repo/models/metadata/session"; // Adjust the import path as necessary
 export async function getSession(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
-
+    
     const name = request.query.get('name') || await request.text() || 'world';
 
     return { body: `Hello, ${name}!` };
